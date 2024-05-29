bf8651bddcf5ac5af2e45d5085f225dffa7598d8
Santiago <santiagocordoba64@gmail.com>
2024-05-29 17:17:09 -0500

    Se configura el eliminar usuario

--- src/components/layouts/Listado/ListadoUsuario.jsx
+++ src/components/layouts/Listado/ListadoUsuario.jsx
@@ -1,63 +1,79 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-import Principal from '../Home/Principal';
-import { collection, getDocs } from 'firebase/firestore';
-import { connDatabase } from '../../database/firebaseConfig';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Principal from "../Home/Principal";
+import { collection, getDocs, doc, deleteDoc } from "firebase/firestore";
+import { connDatabase } from "../../database/firebaseConfig";
+import Swal from "sweetalert2";
 
 
 const ListadoUsuario = () => {
-    const [usuarios, setUsuarios] = useState([]);
-    let redireccion = useNavigate()
-    async function getUsuarios() {
-        let resultado = collection(connDatabase, "usuarios");
-        let data = await getDocs(resultado);
-        /* Si es un arreglo, puedo iterarlo con los métodos de JS
+  const [usuarios, setUsuarios] = useState([]);
+  let redireccion = useNavigate();
+  async function getUsuarios() {
+    let resultado = collection(connDatabase, "usuarios");
+    let data = await getDocs(resultado);
+    /* Si es un arreglo, puedo iterarlo con los métodos de JS
         map */
-        console.log(data.docs);
-        console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-        setUsuarios(data.docs.map((doc) => ({ ...doc.data(), id: doc.id})));
-    }
-    useEffect(() => {
-        getUsuarios();
-    }, []);
+    console.log(data.docs);
+    console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    setUsuarios(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  }
+  useEffect(() => {
+    getUsuarios();
+  }, []);
 
-    const eliminarUsuario = (id) => {
-        console.log('Eliminando el usuario ' + id)
-    }
+  const eliminarUsuario = async (id) => {
+    console.log("Eliminando el usuario " + id);
+    let deleteUser = doc(connDatabase, "usuarios", id);
+    await deleteDoc(deleteUser);
+    getUsuarios();
+  };
 
-    // const confirmar(){
-        
-    // }
+  function confirmar() {
+    Swal.fire({
+      title: "Estás seguro?",
+      text: "No se puede reversar esta acción",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, eliminar!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        eliminarUsuario(id);
+        Swal.fire({
+          title: "Elimnado!",
+          text: "EL usuario fue eliminado.",
+          icon: "success",
+        });
+      }
+    });
+  }
 
-    return (
-        <section className="panel">
-            {/* <Principal /> */}
-            <main className="panel-contenido">
-                {
-                    usuarios.map((element) => (
-                        <section>
+  return (
+    <section className="panel">
+      {/* <Principal /> */}
+      <main className="panel-contenido">
+        {usuarios.map((element) => (
+          <section>
+            <section>
+              <section>
+                <p>Nombre: {element.name}</p>
+                <p>User: {element.user}</p>
+                <p>Password: {element.password}</p>
+                <p>Correo: {element.email}</p>
+              </section>
+              <div>
+                <button onClick={() => confirmar(element.id)}>Eliminar</button>
 
-                            <section>
-
-                                <section>
-                                    <p>Nombre: {element.name}</p>
-                                    <p>User: {element.user}</p>
-                                    <p>Password: {element.password}</p>
-                                    <p>Correo: {element.mail}</p>
-                                </section>
-                                <div>
-                                    <button onClick={() => eliminarUsuario(element.id)}>Eliminar</button>
-
-                                    {/* <button onClick={()=>editarUsuario()}>Editar</button> */}
-                                </div>
-                            </section>
-                        </section>
-                    ))
-                }
-
-            </main>
-        </section>
-    );
+                <button onClick={() => editarUsuario()}>Editar</button>
+              </div>
+            </section>
+          </section>
+        ))}
+      </main>
+    </section>
+  );
 };
 
-export default ListadoUsuario
+export default ListadoUsuario;
