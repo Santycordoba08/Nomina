3577bc621dfb1515ae3c50fe7ac41679c468573f
Santiago <santiagocordoba64@gmail.com>
2024-06-13 15:52:08 -0500

    Se configura componente Comprobante

--- src/components/layouts/ComprobantePago/Comprobante.jsx
+++ src/components/layouts/ComprobantePago/Comprobante.jsx
@@ -1,127 +1,61 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
-import './Comprobante.css'; // Asegúrate de crear y enlazar este archivo CSS
+import React, { useEffect, useState } from 'react';
+import './Comprobante.css'; // Asegúrate de que este archivo existe y está bien vinculado
 
 const Comprobante = () => {
-    const location = useLocation();
-    const { result } = location.state || {};
+    const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        const savedResults = JSON.parse(localStorage.getItem('liquidationResults')) || [];
+        setResults(savedResults);
+    }, []);
 
     return (
-        <div className="comprobante">
-            <h2>Comprobante de Pago</h2>
-            {result ? (
-                <div className="comprobante-detalle">
-                    <p><span>Nombre:</span> {result.name}</p>
-                    <p><span>Apellidos:</span> {result.apellidos}</p>
-                    <p><span>Documento:</span> {result.documento}</p>
-                    <p><span>Días trabajados:</span> {result.daysWorked}</p>
-                    <p><span>Salario proporcional:</span> ${result.proportionalSalary.toFixed(2)}</p>
-                    <p><span>Auxilio de transporte proporcional:</span> ${result.proportionalTransportAllowance.toFixed(2)}</p>
-                    <p><span>Salario total:</span> ${result.totalSalary.toFixed(2)}</p>
-                    <p><span>Salario diario:</span> ${result.dailySalary.toFixed(2)}</p>
-                </div>
-            ) : (
-                <p>No hay datos para mostrar.</p>
-            )}
+        <div className="Fondo-comprobante">
+            <div className="comprobante">
+                <h2 >Comprobante de Pago</h2>
+                {results.length > 0 ? (
+                    results.map((result, index) => (
+                        <div key={index} className="comprobante-detalle">
+                            <div className="comprobante-item">
+                                <span className="comprobante-label">Nombre:</span>
+                                <span className="comprobante-value">{result.name}</span>
+                            </div>
+                            <div className="comprobante-item">
+                                <span className="comprobante-label">Apellidos:</span>
+                                <span className="comprobante-value">{result.apellidos}</span>
+                            </div>
+                            <div className="comprobante-item">
+                                <span className="comprobante-label">Documento:</span>
+                                <span className="comprobante-value">{result.documento}</span>
+                            </div>
+                            <div className="comprobante-item">
+                                <span className="comprobante-label">Días trabajados:</span>
+                                <span className="comprobante-value">{result.daysWorked}</span>
+                            </div>
+                             {/* <div className="comprobante-item">
+                                <span className="comprobante-label">Salario proporcional:</span>
+                                <span className="comprobante-value">${result.proportionalSalary.toFixed(2)}</span>
+                            </div> */}
+                            {/* <div className="comprobante-item">
+                                <span className="comprobante-label">Auxilio de transporte proporcional:</span>
+                                <span className="comprobante-value">${result.proportionalTransportAllowance.toFixed(2)}</span>
+                            </div> */}
+                            {/* <div className="comprobante-item">
+                                <span className="comprobante-label">Salario total:</span>
+                                <span className="comprobante-value">${result.totalSalary.toFixed(2)}</span>
+                            </div>  */}
+                            <div className="comprobante-item">
+                                <span className="comprobante-label">Salario diario:</span>
+                                <span className="comprobante-value">${result.dailySalary.toFixed(2)}</span>
+                            </div>
+                        </div>
+                    ))
+                ) : (
+                    <p>No hay datos para mostrar.</p>
+                )}
+            </div>
         </div>
     );
 };
 
 export default Comprobante;
-
-// ***********************************************************
-
-// import React from 'react';
-// import { useLocation } from 'react-router-dom';
-// import './Comprobante.css'; // Asegúrate de crear y enlazar este archivo CSS
-
-// const Comprobante = () => {
-//     const location = useLocation();
-//     const { result } = location.state || {};
-
-//     return (
-//         <div className="comprobante-container">
-//             <h2 className="comprobante-title">Comprobante de Pago</h2>
-//             {result ? (
-//                 <div className="comprobante-detalle">
-//                     <p><span>Nombre:</span> {result.name}</p>
-//                     <p><span>Apellidos:</span> {result.apellidos}</p>
-//                     <p><span>Documento:</span> {result.documento}</p>
-//                     <p><span>Días trabajados:</span> {result.daysWorked}</p>
-//                     <p><span>Salario proporcional:</span> ${result.proportionalSalary.toFixed(2)}</p>
-//                     <p><span>Auxilio de transporte proporcional:</span> ${result.proportionalTransportAllowance.toFixed(2)}</p>
-//                     <p><span>Salario total:</span> ${result.totalSalary.toFixed(2)}</p>
-//                     <p><span>Salario diario:</span> ${result.dailySalary.toFixed(2)}</p>
-//                 </div>
-//             ) : (
-//                 <p className="no-data-message">No hay datos para mostrar.</p>
-//             )}
-//         </div>
-//     );
-// };
-
-// export default Comprobante;
-
-
-
-
-
-// ******************************************************+
-
-// import React, { useState, useEffect } from 'react';
-// import { useLocation } from 'react-router-dom';
-// import './Comprobante.css'; // Asegúrate de crear y enlazar este archivo CSS
-
-// const Comprobante = () => {
-//     const location = useLocation();
-//     const { result } = location.state || {};
-
-//     const [results, setResults] = useState([]);
-
-//     useEffect(() => {
-//         // Retrieve results from localStorage
-//         const savedResults = JSON.parse(localStorage.getItem('liquidationResults')) || [];
-//         setResults(savedResults);
-//     }, []);
-
-//     const formatNumber = (number) => {
-//         return number !== null && number !== undefined ? number.toFixed(2) : '0.00';
-//     };
-
-//     return (
-//         <div className="comprobante">
-//             <h2>Comprobante de Pago</h2>
-//             {result ? (
-//                 <div className="comprobante-detalle">
-//                     <p><span>Nombre:</span> {result.name}</p>
-//                     <p><span>Apellidos:</span> {result.apellidos}</p>
-//                     <p><span>Documento:</span> {result.documento}</p>
-//                     <p><span>Días trabajados:</span> {result.daysWorked}</p>
-//                     <p><span>Salario proporcional:</span> ${formatNumber(result.proportionalSalary)}</p>
-//                     <p><span>Auxilio de transporte proporcional:</span> ${formatNumber(result.proportionalTransportAllowance)}</p>
-//                     <p><span>Salario total:</span> ${formatNumber(result.totalSalary)}</p>
-//                     <p><span>Salario diario:</span> ${formatNumber(result.dailySalary)}</p>
-//                 </div>
-//             ) : (
-//                 <p>No hay datos para mostrar.</p>
-//             )}
-//             <h3>Historial de Liquidaciones</h3>
-//             <div className="historial">
-//                 {results.map((res, index) => (
-//                     <div key={index} className="comprobante-detalle">
-//                         <p><span>Nombre:</span> {res.name}</p>
-//                         <p><span>Apellidos:</span> {res.apellidos}</p>
-//                         <p><span>Documento:</span> {res.documento}</p>
-//                         <p><span>Días trabajados:</span> {res.daysWorked}</p>
-//                         <p><span>Salario proporcional:</span> ${formatNumber(res.proportionalSalary)}</p>
-//                         <p><span>Auxilio de transporte proporcional:</span> ${formatNumber(res.proportionalTransportAllowance)}</p>
-//                         <p><span>Salario total:</span> ${formatNumber(res.totalSalary)}</p>
-//                         <p><span>Salario diario:</span> ${formatNumber(res.dailySalary)}</p>
-//                     </div>
-//                 ))}
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default Comprobante;
--- src/components/layouts/Nomina/Liquidacion.jsx
+++ src/components/layouts/Nomina/Liquidacion.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import dayjs from 'dayjs';
 import { useNavigate, useLocation } from 'react-router-dom';
-import '../../../index.css'; // Asegúrate de que la ruta sea correcta
+import '../../../index.css'; // Ensure this path is correct
 
 const Liquidacion = () => {
     const location = useLocation();
@@ -12,7 +12,6 @@ const Liquidacion = () => {
     const [salary, setSalary] = useState(0);
     const [salrayextra, setsalrayextra] = useState(0);
     const [hasTransportAllowance, setHasTransportAllowance] = useState(false);
-    const [result, setResult] = useState(null);
     const navigate = useNavigate();
 
     const calculate = () => {
@@ -40,12 +39,12 @@ const Liquidacion = () => {
         const updatedResults = [...existingResults, calculationResult];
         localStorage.setItem('liquidationResults', JSON.stringify(updatedResults));
 
-        navigate('/comprobante', { state: { result: calculationResult } });
+        navigate('/comprobante');
     };
 
     return (
         <div className='Liquidacion'>
-            <h1>Mi Calculadora</h1>
+            <h1 className='titulo-calculadora'>Mi Calculadora</h1>
             <p>¡Calcula tus Prestaciones para {name} {apellidos} (ID: {documento})!</p>
             <div>
                 1. FECHA DE INICIO DEL PERIODO A LIQUIDAR
@@ -81,90 +80,3 @@ const Liquidacion = () => {
 };
 
 export default Liquidacion;
-
-
-
-// ************************************************************
-
-// import React, { useState } from 'react';
-// import dayjs from 'dayjs';
-// import { useNavigate } from 'react-router-dom';
-// import  '../../../index.css'; // Asegúrate de que la ruta sea correcta
-
-// const Liquidacion = () => {
-//     const [fechainicio, setfechainicio] = useState('');
-//     const [fechafinal, setfechafinal] = useState('');
-//     const [salary, setSalary] = useState(0);
-//     const [salrayextra, setsalrayextra] = useState(0);
-//     const [hasTransportAllowance, setHasTransportAllowance] = useState(false);
-//     const navigate = useNavigate();
-
-//     const calculate = () => {
-//         const start = dayjs(fechainicio);
-//         const end = dayjs(fechafinal);
-//         const daysWorked = end.diff(start, 'day') + 1;
-//         const dailySalary = salary / 30;
-//         const proportionalSalary = dailySalary * daysWorked;
-//         const transportAllowance = hasTransportAllowance ? 106454 : 0;
-//         const proportionalTransportAllowance = (transportAllowance / 30) * daysWorked;
-//         const totalSalary = proportionalSalary + proportionalTransportAllowance + parseFloat(salrayextra);
-
-//         const calculationResult = {
-//             daysWorked,
-//             proportionalSalary,
-//             proportionalTransportAllowance,
-//             totalSalary,
-//             dailySalary,
-//             id: new Date().getTime() // Unique identifier for each result
-//         };
-
-//         // Retrieve existing results from localStorage
-//         const existingResults = JSON.parse(localStorage.getItem('liquidationResults')) || [];
-
-//         // Add new result to the existing results
-//         const updatedResults = [...existingResults, calculationResult];
-
-//         // Save updated results back to localStorage
-//         localStorage.setItem('liquidationResults', JSON.stringify(updatedResults));
-
-//         navigate('/comprobante', { state: { result: calculationResult } });
-//     };
-
-//     return (
-//         <div className='Liquidacion'>
-//             <h1>Mi Calculadora</h1>
-//             <p>¡Calcula tus Prestaciones!</p>
-//             <div>
-//                 1. FECHA DE INICIO DEL PERIODO A LIQUIDAR
-//                 <input type="date" value={fechainicio} onChange={(e) => setfechainicio(e.target.value)} />
-//             </div>
-//             <br />
-//             <div>
-//                 2. FECHA DEL FINAL DEL PERIODO A LIQUIDAR
-//                 <input type="date" value={fechafinal} onChange={(e) => setfechafinal(e.target.value)} />
-//             </div>
-//             <br />
-//             <div>
-//                 3. INGRESE SU SALARIO MENSUAL
-//                 <input type="number" value={salary} onChange={(e) => setSalary(parseFloat(e.target.value))} />
-//             </div>
-//             <br />
-//             <div>
-//                 4. ¿TIENE DERECHO A UN AUXILIO DE TRANSPORTE?
-//                 <select value={hasTransportAllowance} onChange={(e) => setHasTransportAllowance(e.target.value === 'true')}>
-//                     <option value="true">Sí</option>
-//                     <option value="false">No</option>
-//                 </select>
-//             </div>
-//             <br />
-//             <div>
-//                 5. ¿Tiene horas extras?
-//                 <input type="number" value={salrayextra} onChange={(e) => setsalrayextra(parseFloat(e.target.value))} />
-//             </div>
-//             <br />
-//             <button onClick={calculate}>Calcular</button>
-//         </div>
-//     );
-// };
-
-// export default Liquidacion;
